b62023c39fc38e7cd0591cc61e837a4495c1f122
G. Roggemans <g.roggemans@grog.be>
2017-02-25 19:44:07 +0100

    Use improved update strategy

--- ellipsis.sh
+++ ellipsis.sh
@@ -7,7 +7,7 @@
 ##############################################################################
 
 # Minimal ellipsis version
-ELLIPSIS_VERSION_DEP='1.9.0'
+ELLIPSIS_VERSION_DEP='1.9.4'
 
 # Package dependencies (informational/not used!)
 ELLIPSIS_PKG_DEPS=''
@@ -36,6 +36,18 @@ pkg.link() {
 
 pkg.init() {
     . "$PKG_PATH/functions.sh"
+}
+
+##############################################################################
+
+pkg.pull() {
+    # Use improved update strategy
+    git remote update 2>&1 > /dev/null
+    if git.is_behind; then
+        pkg.unlink
+        git.pull
+        pkg.link
+    fi
 }
 
 ##############################################################################
